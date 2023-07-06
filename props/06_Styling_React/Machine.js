96c8d1ee7b380261d4e59cb26e8a67f3601643d5
Fredy Velasquez <macacoloco696@gmail.com>
2023-07-06 11:59:18 -0600

    style

--- props/06_Styling_React/Machine.js
+++ props/06_Styling_React/Machine.js
@@ -2,14 +2,15 @@ class Machine extends React.Component {
 	render() {
 		const { s1, s2, s3 } = this.props;
 		const winner = s1 === s2 && s2 === s3;
-		const colors = { fontSize: '50px', backgroundColor: 'purple' };
 
 		return (
-			<div className="Machine">
-				<p style={colors}>
+			<div className="Machine" >
+				<p style={{fontSize}} >
 					{s1} {s2} {s3}
 				</p>
-				<p className={winner ? 'win' : 'lose'}>{winner ? 'Winner!' : 'Loser!'}</p>
+				<p className={winner ? 'win' : 'lose'}>
+					{winner ? 'Winner!' : 'Loser!'}
+				</p>
 			</div>
 		);
 	}
